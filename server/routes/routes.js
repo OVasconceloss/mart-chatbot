aa6ab268b5be6b68d576a8a0e8a119d4e307cf4c
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-24 22:17:04 +0000

    :sparkles: feat: create the API route to access the OpenAI service

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,3 +1,18 @@
+import createOpenAIMessage from "../services/openaiService.js";
+
 export default async function fastifyRoutes (fastify) {
-    
+
+    //ROUTE TO SEND THE USER'S MESSAGE TO THE OPENAI API
+    fastify.get('/:userMessage', async (request, response) => {
+        let previousMessages = [];
+        const { userMessage } = request.params;
+
+        try {
+            const chatMessage = await createOpenAIMessage(userMessage, previousMessages);
+            response.code(200).send({status: 200});
+        } catch (requestError) {
+            console.log(`${requestError}`);
+            response.send({error: requestError});
+        }
+    });
 };
