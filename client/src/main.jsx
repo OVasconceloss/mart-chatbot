2931be9b4e36905e1af4d9b4353707e93db88a64
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-25 10:30:33 +0000

    :sparkles: feat: import the application's route file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,5 @@
 import './styles/index.css';
+import { Router } from './router';
 import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')).render();
+ReactDOM.createRoot(document.getElementById('root')).render( <Router /> );
