e2c90e75fa036bbc40f0097e26ced1c66b62aeb7
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-25 13:16:02 +0000

    :hammer: refactor: use fastify-cors to authorize the frontend. Change the structure of the message history

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,15 +1,22 @@
+import FastifyCors from '@fastify/cors';
 import createOpenAIMessage from "../services/openaiService.js";
 
-export default async function fastifyRoutes(fastify) {
+let messageHistory = [];
 
-    //ROUTE TO SEND THE USER'S MESSAGE TO THE OPENAI API
+export default async function fastifyRoutes(fastify) {
+    fastify.register(FastifyCors, {
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE']
+    });
+    
     fastify.post('/mart', async (request, response) => {
-        const { userMessage, previousMessages } = request.body;
+        const { userMessage } = request.body;
 
         try {
-            const { response: responseChat, messages } = await createOpenAIMessage(userMessage, previousMessages);
-            
-            return { response: responseChat.choices[0], messages };
+            const { response: responseChat, messages } = await createOpenAIMessage(userMessage, messageHistory);
+            messageHistory = messages;
+
+            return { response: responseChat, messages };
         } catch (requestError) {
             console.log(`An error occurred while trying to receive the OpenAI API message: ${requestError}`);
             return requestError;
