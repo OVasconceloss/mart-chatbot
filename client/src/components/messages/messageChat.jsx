f35f59fd88ac1722cad802b65f9a86347de30d2a
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-25 16:42:22 +0000

    :hammer: refactor: create the verification logic for the newest message

--- client/src/components/messages/messageChat.jsx
+++ client/src/components/messages/messageChat.jsx
@@ -1,22 +1,26 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from "react"
 
-export const MessageChat = ({chatMessage}) => {
+export const MessageChat = ({ chatMessage, isLatest }) => {
     const [displayMessage, setDisplayMessage] = useState('');
 
     useEffect(() => {
-        let currentCharIndex = 0;
-        const interval = setInterval(() => {
-            if (currentCharIndex <= chatMessage.length) {
-                setDisplayMessage(chatMessage.slice(0, currentCharIndex));
-                currentCharIndex++;
-            } else {
-                clearInterval(interval);
-            }
-        }, 50);
+        if (isLatest) {
+            let currentCharIndex = 0;
+            const interval = setInterval(() => {
+                if (currentCharIndex <= chatMessage.length) {
+                    setDisplayMessage(chatMessage.slice(0, currentCharIndex));
+                    currentCharIndex++;
+                } else {
+                    clearInterval(interval);
+                }
+            }, 50);
 
-        return () => clearInterval(interval);
-    }, [chatMessage]);
+            return () => clearInterval(interval);
+        } else {
+            setDisplayMessage(chatMessage);
+        }
+    }, [chatMessage, isLatest]);
 
     return (
         <div id="message-mart" className="flex items-start p-10 gap-3">
