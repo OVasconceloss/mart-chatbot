14592e6e80bc9e3d30630ab0af3f25b6c59d9815
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-25 12:17:03 +0000

    :lisptick: style: style the section elements

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,8 +1,8 @@
 const Home = () => {
     return (
         <>
-        <main className="flex w-screen h-screen bg-zinc-800">
-            <aside className="flex flex-col w-96 h-full p-5 bg-zinc-900 rounded-r-xl">
+        <main className="flex justify-between h-screen bg-zinc-800">
+            <aside className="flex flex-col w-64 h-full p-5 bg-zinc-900 rounded-r-xl">
                 <div id="header-aside">
                     <h1 className="pb-5 mb-5 text-2xl text-zinc-100 text-center">M.A.R.T</h1>
                 </div>
@@ -25,7 +25,7 @@ const Home = () => {
                     </div>
                 </div>
             </aside>
-            <section className="w-full p-5 bg-zinc-800">
+            <section className="flex flex-col w-full h-screen p-5 bg-zinc-800">
                 <div id="header-section" className="flex items-center justify-between w-full">
                     <h1 className="text-2xl text-zinc-100">GPT 3.5 Turbo</h1>
                     <div>
@@ -33,16 +33,64 @@ const Home = () => {
                             <button 
                                 className="w-20 border border-zinc-100 rounded-md text-xl text-zinc-100
                                 transition ease-linear hover:bg-zinc-100 hover:text-zinc-800">
-                                <i className="fa-brands fa-github"></i>
+                                    <i className="fa-brands fa-github"></i>
                             </button>
                         </a>
                     </div>
                 </div>
-                <div id="messages">
-
+                <div id="messages" className="flex-grow overflow-y-auto m-10">
+                    <div id="message-user" className="flex items-start p-10 gap-3">
+                        <img 
+                            src="https://github.com/OVasconceloss.png" 
+                            alt="Profile Picture"
+                            className="w-10 h-10 rounded-full object-cover"
+                        />
+                        <div>
+                            <h4 className="text-lg text-zinc-100">User Name</h4>
+                            <p className="py-2 text-zinc-100 text-justify">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                Laboriosam eum natus consectetur incidunt fugit aut eaque ex, 
+                                quas odio perferendis minus dolorum eligendi quasi amet sapiente 
+                                nisi qui minima eveniet!
+                            </p>
+                        </div>
+                    </div>
+                    <div id="message-mart" className="flex items-start p-10 gap-3">
+                        <img 
+                            src="https://github.com/rocketseat.png" 
+                            alt="Profile Picture"
+                            className="w-10 h-10 rounded-full object-cover"
+                        />
+                        <div>
+                            <h4 className="text-lg text-zinc-100">M.A.R.T</h4>
+                            <p className="py-2 text-zinc-100 text-justify">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                Laboriosam eum natus consectetur incidunt fugit aut eaque ex, 
+                                quas odio perferendis minus dolorum eligendi quasi amet sapiente 
+                                nisi qui minima eveniet!
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                Laboriosam eum natus consectetur incidunt fugit aut eaque ex, 
+                                quas odio perferendis minus dolorum eligendi quasi amet sapiente 
+                                nisi qui minima eveniet!
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                Laboriosam eum natus consectetur incidunt fugit aut eaque ex, 
+                                quas odio perferendis minus dolorum eligendi quasi amet sapiente 
+                                nisi qui minima eveniet!
+                            </p>
+                        </div>
+                    </div>
                 </div>
-                <div id="user-input">
-
+                <div id="user-input" className="flex items-center justify-center w-full">
+                    <input 
+                        type="text"
+                        placeholder="Message Mart Chatbot"
+                        className="w-[46rem] my-5 p-2 bg-zinc-700 outline-none border border-zinc-700 rounded-md text-zinc-100
+                        transition ease-linear focus:border-zinc-100"
+                    />
+                    <button className="w-10 mx-2 p-2 bg-zinc-700 rounded-md text-zinc-100
+                    transition ease-linear hover:bg-zinc-100 hover:text-zinc-700">
+                        <i className="fa-solid fa-paper-plane"></i>
+                    </button>
                 </div>
             </section>
         </main>
