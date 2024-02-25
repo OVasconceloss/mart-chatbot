978170d09f17e7b80b4a6e878084d28f3e442930
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-25 10:39:37 +0000

    :sparkles: feat: create the basic structure of the home page

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,7 +1,41 @@
 const Home = () => {
     return (
         <>
-        
+        <main>
+            <aside>
+                <div id="header-aside">
+                    <h1>M.A.R.T</h1>
+                </div>
+                <div id="content-aside">
+                    <div>
+                        <h2>Chat Name Example</h2>
+                        <button>...</button>
+                    </div>
+                </div>
+                <div id="footer-aside">
+                    <div>
+                        <img src="https://github.com/OVasconceloss.png" alt="Profile Picture" />
+                        <p>User Name Example</p>
+                    </div>
+                </div>
+            </aside>
+            <section>
+                <div id="header-section">
+                    <h1>GPT 3.5 Turbo</h1>
+                    <div>
+                        <a href="https://github.com/OVasconceloss/mart-chatbot" target="_blank">
+                            <button>Github</button>
+                        </a>
+                    </div>
+                </div>
+                <div id="messages">
+
+                </div>
+                <div id="user-input">
+
+                </div>
+            </section>
+        </main>
         </>
     );
 };
