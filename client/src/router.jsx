9681bf6b71c33ebee253deb0d6550fb7f64a2e9e
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-25 10:32:29 +0000

    :sparkles: feat: create the application's main route

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,9 +1,11 @@
-import { BrowserRouter, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from './pages/home';
 
 export const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path='/' element={ <Home /> } />
             </Routes>
         </BrowserRouter>
     )
