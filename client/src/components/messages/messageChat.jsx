527d00c154bbe85580f364233b345284a23a9f75
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-25 16:18:51 +0000

    :sparkles: feat: create the 'streaming' effect in the chatbot's response

--- client/src/components/messages/messageChat.jsx
+++ client/src/components/messages/messageChat.jsx
@@ -1,5 +1,23 @@
-// eslint-disable-next-line react/prop-types
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react"
+
 export const MessageChat = ({chatMessage}) => {
+    const [displayMessage, setDisplayMessage] = useState('');
+
+    useEffect(() => {
+        let currentCharIndex = 0;
+        const interval = setInterval(() => {
+            if (currentCharIndex <= chatMessage.length) {
+                setDisplayMessage(chatMessage.slice(0, currentCharIndex));
+                currentCharIndex++;
+            } else {
+                clearInterval(interval);
+            }
+        }, 50);
+
+        return () => clearInterval(interval);
+    }, [chatMessage]);
+
     return (
         <div id="message-mart" className="flex items-start p-10 gap-3">
             <img 
@@ -9,7 +27,7 @@ export const MessageChat = ({chatMessage}) => {
             />
             <div>
                 <h4 className="text-lg text-zinc-100">M.A.R.T</h4>
-                <p className="py-2 text-zinc-100 text-justify">{chatMessage}</p>
+                <p className="py-2 text-zinc-100 text-justify">{displayMessage}</p>
             </div>
         </div>
     )
