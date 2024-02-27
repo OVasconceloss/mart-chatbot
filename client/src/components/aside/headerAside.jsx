9eb54da8f409edc85f95cbf02f7f8913109ac436
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-27 08:46:39 +0000

    :sparkles: feat: create the component for the aside header

---
+++ client/src/components/aside/headerAside.jsx
@@ -0,0 +1,7 @@
+export const HeaderAside = () => {
+    return (
+        <div id="header-aside">
+            <h1 className="pb-5 mb-5 text-2xl text-zinc-100 text-center">M.A.R.T</h1>
+        </div>
+    );
+};
